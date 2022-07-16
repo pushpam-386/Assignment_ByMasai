7e26182eb20d354c971e63dcfd1631d493fca7c7
pushpamtfs@gmail.com <pushpamtfs@gmail.com>
2022-07-17 03:15:19 +0530

    u-3,sp-4,day-2

--- unit-3/sprint-4/day-2/assignments/question3/src/Bank.java
+++ unit-3/sprint-4/day-2/assignments/question3/src/Bank.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Bank {
+public class Bank {
+    private Bank(){
+        System.out.println("Inside Private Constructor of Bank");
+    }
+
+    static void setBank()
+    {
+        Bank b = new Bank();
+    }
 }
