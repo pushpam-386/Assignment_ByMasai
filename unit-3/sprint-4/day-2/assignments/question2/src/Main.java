6013de33798f627612f95333df51d3e9c8645e06
pushpamtfs@gmail.com <pushpamtfs@gmail.com>
2022-07-17 02:16:55 +0530

    sprint4,day-3

---
+++ unit-3/sprint-4/day-2/assignments/question2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static Employee getEmployeeDetails(){
+
+    }
+
+    public static int getPFPercentage(){
+        
+    }
+    public static void main(String[] args) {
+    }
+}
