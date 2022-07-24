77f115c71c456c26ddcee1c960d5c08c203403a3
pushpamtfs@gmail.com <pushpamtfs@gmail.com>
2022-07-24 20:58:03 +0530

    sprint-5,assign day-2

--- unit-3/sprint-5/day-2/assignment/question-1/src/Main.java
+++ unit-3/sprint-5/day-2/assignment/question-1/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EmployeeBonus employeeBonus=new EmployeeBonus();
+        try {
+            employeeBonus.bonus("20");
+        }
+        catch (InvalidAgeexception invalidAgeexception){
+            System.out.println(invalidAgeexception.getMessage());
+        }
     }
 }
