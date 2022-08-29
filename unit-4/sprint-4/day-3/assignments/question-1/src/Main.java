bae469c804562cb1a49a4777f8b6ffe576081142
pushpamtfs@gmail.com <pushpamtfs@gmail.com>
2022-08-29 11:04:11 +0530

    ass sp-4,unit-4,Transactions_Db Schema

--- unit-4/sprint-4/day-3/assignments/question-1/src/Main.java
+++ unit-4/sprint-4/day-3/assignments/question-1/src/Main.java
@@ -1,5 +1,40 @@
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+        String url= "jdbc:mysql://localhost:3306";
+
+        try {
+            Connection conn= DriverManager.getConnection(url, "root", "root");
+
+            if(conn  != null)
+                System.out.println("connected...");
+
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
+
 }
+
+
+
+
+
