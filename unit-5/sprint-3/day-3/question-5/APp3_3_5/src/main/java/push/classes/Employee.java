6f838212cc5bb00682bf788adadc3e3d0b3ef2d6
pushpamtfs@gmail.com <pushpamtfs@gmail.com>
2022-09-18 16:03:56 +0530

    ass 3rd sp-3 unit 5 spring configuraration bean by xml

---
+++ unit-5/sprint-3/day-3/question-5/APp3_3_5/src/main/java/push/classes/Employee.java
@@ -0,0 +1,50 @@
+package push.classes;
+
+public class Employee {
+
+	private int empId;
+	private String empName;
+	private String address;
+	private int salary;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", address=" + address + ", salary=" + salary
+				+ "]";
+	}
+
+
+	public Employee() {
+		super();
+	}
+
+
+	public Employee(int empId, String empName, String address, int salary) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.address = address;
+		this.salary = salary;
+	}
+	
+	
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	
+	
+	
+
+}
