45effcb0f1011c7c627af3fdb73aa0cc702ac054
pushpamtfs@gmail.com <pushpamtfs@gmail.com>
2022-11-03 18:53:09 +0530

    sp-4 order

---
+++ RJ-101/sp-4/movieBookingApplication/movieBookingApp_07/src/main/java/com/push/MovieBookingApp07Application.java
@@ -0,0 +1,13 @@
+package com.push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingApp07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBookingApp07Application.class, args);
+	}
+
+}
---
+++ RJ-101/sp-4/movieBookingApplication/movieBookingApp_07/src/test/java/com/push/MovieBookingApp07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.push;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBookingApp07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
