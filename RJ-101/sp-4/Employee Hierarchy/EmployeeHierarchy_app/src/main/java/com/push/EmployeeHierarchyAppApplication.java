92f8c6a2e72f8a28e0cbd5d310514249063b1c9c
pushpamtfs@gmail.com <pushpamtfs@gmail.com>
2022-11-09 15:33:35 +0530

    e-commerce

---
+++ RJ-101/sp-4/Employee Hierarchy/EmployeeHierarchy_app/src/main/java/com/push/EmployeeHierarchyAppApplication.java
@@ -0,0 +1,13 @@
+package com.push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeHierarchyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeHierarchyAppApplication.class, args);
+	}
+
+}
