0db933f55fd8d54d5f3f01aa9605a9b2636ecf22
pushpamtfs@gmail.com <pushpamtfs@gmail.com>
2022-11-17 02:20:20 +0530

    miniproject start

---
+++ RJ-101/sp-rep-1/mini-project(e-commerce)/EcommerceApp_1/src/main/java/com/push/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.push.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
---
+++ RJ-101/sp-rep-1/mini-project(e-commerce)/EcommerceApp_1/src/main/java/com/push/models/Product.java
@@ -0,0 +1,5 @@
+package com.push.models;
+
+public class Product {
+
+}
